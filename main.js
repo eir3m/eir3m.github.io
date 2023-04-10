ed99a55871f9768fa72b02c1e5a3abebdd08d43c
12312321 <77736987+eir3m@users.noreply.github.com>
2023-04-10 08:11:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
-async function copydiscord() {
-    try {
-        await navigator.clipboard.writeText('Xissayala#5557');
-        ds.style.display='block';
-        setTimeout("ds.style.display='none'", 2000);
-    }
-    catch (err) {
-        console.error('Ошибка: ', err);
-        alert('Xissayala#5557')
-    }
+// Импортируем Three.js
+import * as THREE from 'three';
+
+// Создаем сцену
+const scene = new THREE.Scene();
+
+// Создаем камеру
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// Создаем рендерер
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Создаем геометрию для октаэдра
+const geometry = new THREE.OctahedronGeometry(2, 0);
+
+// Создаем материал для октаэдра
+const material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+
+// Создаем объект меша, объединяющий геометрию и материал
+const octahedron = new THREE.Mesh(geometry, material);
+
+// Добавляем октаэдр на сцену
+scene.add(octahedron);
+
+// Создаем анимацию
+function animate() {
+  requestAnimationFrame(animate);
+  octahedron.rotation.x += 0.01;
+  octahedron.rotation.y += 0.01;
+  renderer.render(scene, camera);
 }
+
+animate();
